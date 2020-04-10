1e2644d884dafe5f94e796cc60d6de4f985a74c4
GKCY <frendchen@qq.com>
2020-04-11 03:06:29 +0800

    Daily Update

--- src/No82.java
+++ src/No82.java
@@ -56,16 +56,4 @@ public class No82 {
         return dummy.next;
     }
 
-    public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(3);
-        head.next.next.next.next = new ListNode(4);
-        head.next.next.next.next.next = new ListNode(4);
-        head.next.next.next.next.next.next = new ListNode(5);
-        head.next.next.next.next.next.next.next = null;
-        No82 t = new No82();
-        t.deleteDuplicates(head);
-    }
 }
